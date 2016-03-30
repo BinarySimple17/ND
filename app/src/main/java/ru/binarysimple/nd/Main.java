fbb090758f7e5ebf5ccc06fb63839a81ce2f9315
Binary Simple <shovkovenko@inbox.ru>
2016-03-30 17:27:55 +0300

    1212

--- app/src/main/java/ru/binarysimple/nd/Main.java
+++ app/src/main/java/ru/binarysimple/nd/Main.java
@@ -2,6 +2,9 @@ package ru.binarysimple.nd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        RadioGroup radiogroup = (RadioGroup) findViewById(R.id.rgTaxes);
+
+        radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                EditText etTaxRate = (EditText) findViewById(R.id.etTaxrate);
+                RadioButton rbOther = (RadioButton) findViewById(R.id.rbOther);
+                etTaxRate.setEnabled(rbOther.isChecked());
+
+                // TODO Auto-generated method stub
+                switch (checkedId) {
+                    case -1:
+                        break;
+                    case R.id.rbUsual13:
+
+                        break;
+                    case R.id.rbNonresident30:
+
+                        break;
+                    case R.id.rbDividend9:
+
+                        break;
+
+                    case R.id.rbOther:
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
 }
